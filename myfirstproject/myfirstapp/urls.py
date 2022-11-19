d4a96bb15df456bfb2a820a8e5272037a278d797
carad16 <cesdulay@iskolarngbayan.pup.edu.ph>
2022-11-19 22:22:56 +0800

    Commit JS and CSS Templates

--- myfirstproject/myfirstapp/urls.py
+++ myfirstproject/myfirstapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add/<int:a>/<int:b>', views.add,name="add"),
     path('intro/<str:name>/<int:age>', views.intro,name="intro"),
     path('myfirstpage', views.myfirstpage,name="myfirstpage"),
+    path('mysecondpage', views.mysecondpage,name="mysecondpage"),
+    path('mythirdpage', views.mythirdpage,name="mythirdpage"),
 ]
--- myfirstproject/myfirstapp/views.py
+++ myfirstproject/myfirstapp/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.template import loader
+
+
 # Create your views here.
 def myfunctioncall(request):
     return HttpResponse("Hello World")
@@ -18,4 +21,23 @@ def intro(request,name,age):
     return JsonResponse(mydictionary)  
 
 def myfirstpage(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
+def mysecondpage(request):
+    return render(request, "sample.html")
+
+def mythirdpage(request):
+    var = "hello world"
+    greeting = "hey, how are you?"
+    fruits = ["apple", "mango", "banana"]
+    num1, num2 = 5, 3
+    ans = num1 > num2
+    mydictionary = {
+        "var": var,
+        "msg": greeting,
+        "myfruits": fruits,
+        "num1": num1,
+        "num2": num2,
+        "ans": ans
+    }
+    return render(request, "sample1.html",context=mydictionary) 
