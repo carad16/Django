a733ab259e681c6cded100ec283b5f7a9eb02540
carad16 <cesdulay@iskolarngbayan.pup.edu.ph>
2022-12-24 21:19:34 +0800

    added instructions and submission of query

--- djangoprojects/calculatorapp/views.py
+++ djangoprojects/calculatorapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    return HttpResponse(q)
